31c12b5296493f0ba2402351687700e7347adf90
saverolubi <saverolubi@gmail.com>
2022-03-18 16:52:20 +0700

    Initial commit

---
+++ TPMOD4_1302200004.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMOD4_1302200004
+{
+    public class ClassMain
+    {
+        public static void Main(string[] args)
+        {
+            String nama = "NAMAPANGGILAN";
+            HaloGeneric.SapaUser<string>(ref nama);
+            DataGeneric<string> dataGeneric = new DataGeneric<string>("1302200004");
+            dataGeneric.PrintData();
+        }
+    }
+
+    class HaloGeneric
+    {
+        public static void SapaUser<T>(ref T nama)
+        {
+            Console.WriteLine("Halo user " + nama);
+        }
+    }
+
+    public class DataGeneric<T>
+    {
+        private T Data;
+
+        public DataGeneric(T Data)
+        {
+            this.Data = Data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah " + Data);
+        }
+    }
+}
